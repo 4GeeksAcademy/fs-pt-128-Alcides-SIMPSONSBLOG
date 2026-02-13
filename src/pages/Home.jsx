429b864eba2725187c1dfda15fd5574591f4753f
Alcides Rivera <elarsi626@gmail.com>
2026-02-13 00:23:05 +0000

    First

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,31 @@
+import { useEffect } from "react";
 import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
+import { getCharacters } from "../services/APIservices.jsx";
 
 export const Home = () => {
 
-  const {store, dispatch} =useGlobalReducer()
+	const { store, dispatch } = useGlobalReducer()
+
+	useEffect(()=>{
+		getCharacters(dispatch)
+
+
+	},[])
+
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
+		<div className="container py-4">
+			<div>
+				<h1>Simpsons Characters</h1>
+			</div>
+			<div className="row g-3">
+				{store.characters.map(character =>{
+					<div className="col-sm6 col-mb4 col-lg-3" key={character.id}>
+							card
+					</div>
+				})}
+			</div>
 		</div>
 	);
 }; 
---
+++ src/services/APIservices.jsx
@@ -0,0 +1,11 @@
+export const getCharacters = async (dispatch) => {
+    const response = await fetch(
+        `https://thesimpsonsapi.com/api/characters`)
+    if (!response.ok) {
+        return
+    }
+    const data = await response.json()
+    dispatch({type: "set_characters", payload: data.results})
+    console.log(data.results);
+    
+}
